23dbe0e89e9bde93d910756ade3ef133a1ea27c9
wlsnx <wanlisnx@gmail.com>
2015-04-29 10:55:57 +0800

    add team and many to many relationship of team and league

--- sports/sports_data.py
+++ sports/sports_data.py
@@ -4,6 +4,7 @@
 
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
@@ -19,4 +20,21 @@ class League(Base):
     short_name = sa.Column(sa.String(30))
     short_name_cn = sa.Column(sa.String(30))
     is_cup = sa.Column(sa.Boolean)
+
+
+league_team = sa.Table("league_team",
+                       Base.metadata,
+                       sa.Column("team_id", sa.Integer, sa.ForeignKey("yt_team.id")),
+                       sa.Column("league_id", sa.Integer, sa.ForeignKey("yt_league.id")))
+
+
+class Team(Base):
+
+    __tablename__ = "yt_team"
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String(60))
+    name_en = sa.Column(sa.String(60))
+    city = sa.Column(sa.String(100))
+    league = relationship("League", secondary=league_team, backref="team")
 
