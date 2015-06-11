1e3735aad50630ec324653f1aa642c8c4ba0db48
wlsnx <wanlisnx@gmail.com>
2015-06-11 17:02:51 +0800

    update db

--- sports/sports_data.py
+++ sports/sports_data.py
@@ -45,8 +45,8 @@ league_team = sa.Table("league_team",
 
     #__tablename__ = "league_team"
 
-    #team_id = sa.Column(sa.Integer, sa.ForeignKey("yt_team.id"))
-    #league_id = sa.Column(sa.Integer, sa.ForeignKey("yt_league.id"))
+    #team_id = sa.Column(sa.Integer, default=0, sa.ForeignKey("yt_team.id"))
+    #league_id = sa.Column(sa.Integer, default=0, sa.ForeignKey("yt_league.id"))
 
     #__table_args__ = (sa.PrimaryKeyConstraint(team_id, league_id), )
 
@@ -66,24 +66,24 @@ class Team(Base, Item):
     #espn_name = sa.Column(sa.String(60))
     player = relationship("TeamPlayer")
     #TODO:create player table
-    #master = sa.Column(sa.Integer, sa.ForeignKey("yt_manager.id"))
+    #master = sa.Column(sa.Integer, default=0, sa.ForeignKey("yt_manager.id"))
 
 
 #player_number = sa.Table("player_number",
                          #Base.metadata,
-                         #sa.Column("player_id", sa.Integer, sa.ForeignKey("yt_player.id")),
-                         #sa.Column("team_id", sa.Integer, sa.ForeignKey("yt_team.id")),
-                         #sa.Column("number", sa.SmallInteger))
+                         #sa.Column("player_id", sa.Integer, default=0, sa.ForeignKey("yt_player.id")),
+                         #sa.Column("team_id", sa.Integer, default=0, sa.ForeignKey("yt_team.id")),
+                         #sa.Column("number", sa.SmallInteger, default=0))
 
 
 class TeamPlayer(Base):
 
     __tablename__ = "team_player"
 
-    #id = sa.Column(sa.Integer)
+    #id = sa.Column(sa.Integer, default=0)
     player_id = sa.Column(sa.Integer, sa.ForeignKey("yt_player.id"))
     team_id = sa.Column(sa.Integer, sa.ForeignKey("yt_team.id"))
-    number = sa.Column(sa.SmallInteger)
+    number = sa.Column(sa.SmallInteger, default=0)
 
     __table_args__ = (sa.PrimaryKeyConstraint(player_id, team_id), )
 
@@ -96,14 +96,15 @@ class Player(Base, Item):
     name = sa.Column(sa.String(30))
     name_en = sa.Column(sa.String(30))
     position = sa.Column(sa.String(10))
-    #number = sa.Column(sa.SmallInteger)
+    #number = sa.Column(sa.SmallInteger, default=0)
     height = sa.Column(sa.Float())
     weight = sa.Column(sa.Float())
     country = sa.Column(sa.String(30))
     country_en = sa.Column(sa.String(30))
     birthday = sa.Column(sa.Date())
-    #age = sa.Column(sa.SmallInteger)
+    #age = sa.Column(sa.SmallInteger, default=0)
     team = relationship("TeamPlayer")
+    letter = sa.Column(sa.String(10), default="")
 
 
 class Match(Base, Item):
@@ -111,16 +112,16 @@ class Match(Base, Item):
     __tablename__ = "yt_match"
 
     id = sa.Column(sa.Integer, primary_key=True)
-    espn_id = sa.Column(sa.Integer)
+    espn_id = sa.Column(sa.Integer, default=0)
     league_id = sa.Column(sa.Integer, sa.ForeignKey("yt_league.id"))
-    finish = sa.Column(sa.SmallInteger)
-    home_score = sa.Column(sa.SmallInteger)
-    away_score = sa.Column(sa.SmallInteger)
+    finish = sa.Column(sa.SmallInteger, default=0)
+    home_score = sa.Column(sa.SmallInteger, default=-1)
+    away_score = sa.Column(sa.SmallInteger, default=-1)
     home_id = sa.Column(sa.Integer, sa.ForeignKey("yt_team.id"))
     away_id = sa.Column(sa.Integer, sa.ForeignKey("yt_team.id"))
     date = sa.Column(sa.Date)
     time = sa.Column(sa.Time)
-    m_time = sa.Column(sa.SmallInteger)
+    m_time = sa.Column(sa.SmallInteger, default=0)
 
 
 class MatchFootball(Base, Item):
@@ -129,26 +130,26 @@ class MatchFootball(Base, Item):
 
     id = sa.Column(sa.Integer, primary_key=True)
     match_id = sa.Column(sa.Integer, sa.ForeignKey("yt_match.id"))
-    home_score = sa.Column(sa.SmallInteger)
-    away_score = sa.Column(sa.SmallInteger)
-    home_shot = sa.Column(sa.SmallInteger)
-    away_shot = sa.Column(sa.SmallInteger)
-    home_shots = sa.Column(sa.SmallInteger)
-    away_shots = sa.Column(sa.SmallInteger)
-    home_fouls = sa.Column(sa.SmallInteger)
-    away_fouls = sa.Column(sa.SmallInteger)
-    home_corner = sa.Column(sa.SmallInteger)
-    away_corner = sa.Column(sa.SmallInteger)
-    home_offside = sa.Column(sa.SmallInteger)
-    away_offside = sa.Column(sa.SmallInteger)
-    home_ball_possession = sa.Column(sa.SmallInteger)
-    away_ball_possession = sa.Column(sa.SmallInteger)
-    home_yellow_card = sa.Column(sa.SmallInteger)
-    away_yellow_card = sa.Column(sa.SmallInteger)
-    home_red_card = sa.Column(sa.SmallInteger)
-    away_red_card = sa.Column(sa.SmallInteger)
-    home_saving = sa.Column(sa.SmallInteger)
-    away_saving = sa.Column(sa.SmallInteger)
+    home_score = sa.Column(sa.SmallInteger, default=0)
+    away_score = sa.Column(sa.SmallInteger, default=0)
+    home_shot = sa.Column(sa.SmallInteger, default=0)
+    away_shot = sa.Column(sa.SmallInteger, default=0)
+    home_shots = sa.Column(sa.SmallInteger, default=0)
+    away_shots = sa.Column(sa.SmallInteger, default=0)
+    home_fouls = sa.Column(sa.SmallInteger, default=0)
+    away_fouls = sa.Column(sa.SmallInteger, default=0)
+    home_corner = sa.Column(sa.SmallInteger, default=0)
+    away_corner = sa.Column(sa.SmallInteger, default=0)
+    home_offside = sa.Column(sa.SmallInteger, default=0)
+    away_offside = sa.Column(sa.SmallInteger, default=0)
+    home_ball_possession = sa.Column(sa.SmallInteger, default=0)
+    away_ball_possession = sa.Column(sa.SmallInteger, default=0)
+    home_yellow_card = sa.Column(sa.SmallInteger, default=0)
+    away_yellow_card = sa.Column(sa.SmallInteger, default=0)
+    home_red_card = sa.Column(sa.SmallInteger, default=0)
+    away_red_card = sa.Column(sa.SmallInteger, default=0)
+    home_saving = sa.Column(sa.SmallInteger, default=0)
+    away_saving = sa.Column(sa.SmallInteger, default=0)
     #date = sa.Column(sa.Date)
     #time = sa.Column(sa.Time)
 
@@ -159,9 +160,9 @@ class MatchFootballDetails(Base, Item):
 
     id = sa.Column(sa.Integer, primary_key=True)
     match_id = sa.Column(sa.Integer, sa.ForeignKey("yt_match.id"))
-    min = sa.Column(sa.SmallInteger)
-    team = sa.Column(sa.SmallInteger)
-    type = sa.Column(sa.SmallInteger)
+    min = sa.Column(sa.SmallInteger, default=0)
+    team = sa.Column(sa.SmallInteger, default=0)
+    type = sa.Column(sa.SmallInteger, default=0)
     player_a = sa.Column(sa.String(30))
     player_a_id = sa.Column(sa.Integer, sa.ForeignKey("yt_player.id"))
     player_b = sa.Column(sa.String(30))
@@ -175,14 +176,14 @@ class PlayerMatch(Base, Item):
     id = sa.Column(sa.Integer, primary_key=True)
     match_id = sa.Column(sa.Integer, sa.ForeignKey("yt_match.id"))
     player_id = sa.Column(sa.Integer, sa.ForeignKey("yt_player.id"))
-    fouls_commited = sa.Column(sa.SmallInteger)
-    fouls_suffered = sa.Column(sa.SmallInteger)
-    red_card = sa.Column(sa.SmallInteger)
-    yellow_card = sa.Column(sa.SmallInteger)
-    assists = sa.Column(sa.SmallInteger)
-    goals = sa.Column(sa.SmallInteger)
-    shots = sa.Column(sa.SmallInteger)
-    shots_goal = sa.Column(sa.SmallInteger)
-    offside = sa.Column(sa.SmallInteger)
-    saving = sa.Column(sa.SmallInteger)
+    fouls_commited = sa.Column(sa.SmallInteger, default=0)
+    fouls_suffered = sa.Column(sa.SmallInteger, default=0)
+    red_card = sa.Column(sa.SmallInteger, default=0)
+    yellow_card = sa.Column(sa.SmallInteger, default=0)
+    assists = sa.Column(sa.SmallInteger, default=0)
+    goals = sa.Column(sa.SmallInteger, default=0)
+    shots = sa.Column(sa.SmallInteger, default=0)
+    shots_goal = sa.Column(sa.SmallInteger, default=0)
+    offside = sa.Column(sa.SmallInteger, default=0)
+    saving = sa.Column(sa.SmallInteger, default=0)
 
